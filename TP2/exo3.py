a5c6940fa6a45bd18a31d34798f87d39726f8f94
Jean-Christophe-BURNOT <jean-christophe.burnot@cpe.fr>
2022-05-08 18:38:25 +0200

    commit d'avancé

--- TP2/exo3.py
+++ TP2/exo3.py
@@ -4,5 +4,41 @@
 Created on Sat May  7 16:38:18 2022
 
 @author: terramotu
+Ceci est le code de l'exercice 3 du TP de CS-PC
 """
+import os, sys
 
+if len(sys.argv) <2:
+    print("Préciser les programmes à éxécuter en ligne de commande")
+    sys.exit(1)
+
+while type!=1 and type!=2:
+    type=int(
+        input("Entrez le type d'exécution:1 pour serie 2 pour parrallèle:\n"))
+
+commandes = sys.argv[1:]
+if type==1:
+    for cmd in commandes:
+        try:
+            os.execlp(cmd, cmd)
+        except:
+            print("echec de execlp")
+            
+    
+elif type==2:
+    for cmd in commandes:
+        pid=os.fork()
+        if pid==0:
+            try:
+                os.execlp(cmd, cmd)
+            except:
+                print("echec de execlp")
+                sys.exit(2)
+
+"""
+portion de code qui pourraiit permettre de
+
+if len(liste)>0:
+    print("Commande non exécutées=", liste)
+sys.exit(0)
+"""
