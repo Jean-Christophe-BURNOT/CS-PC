911aad57c413ddc629197ec8789baa099f7edb99
Jean-Christophe-BURNOT <jean-christophe.burnot@cpe.fr>
2022-05-29 16:01:20 +0200

    Casi fin des TP reste séma

--- TP5/exo2.py
+++ TP5/exo2.py
@@ -6,35 +6,56 @@ Created on Sat May 28 11:31:42 2022
 @author: terramotu
 Ceci est l'exercice 2 sur les sémaphores
 """
-import os, time
+import os , sys, time, signal
 import multiprocessing as mp
 
-somme = mp.Value("i",0)
-sem = mp.Semaphore(1)
-pid = os.fork()
-n1 = 0
-n2 = 0
-#processus fils
-if pid==0:
-    for i in range(4):
-        n1 += i
-        print("Je somme les indices")
-        time.sleep(1)
-    sem.acquire()
-    somme.value+=n1
-    print("Le fils a terminé")
-    sem.release()
-    #sys.exit(0)
+Somme = mp.Value('i', 0)
+listeNumber = [4, 6, 2, 1, 9, 7]
+N = len(listeNumber)
+S1 = mp.Semaphore(0)
+S2 = mp.Semaphore(0)
+S = mp.Semaphore(1)
 
-else:
-    #os.wait()
-    for i in range(4):
-        n1 += i
-        print("Je somme mais je suis le second")
-        time.sleep(1)
-    sem.acquire()
-    somme.value+=n1
-    print("Le père a terminé")
-    sem.release()
+def SommeImp(listeNumber, N):
+    SommeImpairs = 0
+    i = 1
     
-print(somme.value)
+    while i<= N :
+        S1.release()
+        S2.acquire()
+        SommeImpairs = SommeImpairs + listeNumber[i]
+        i += 2
+        print('Compteur Impaire =', i)
+    S.acquire()    
+    Somme.value += SommeImpairs
+    S.release()
+    print(Somme.value)
+    S.acquire()
+    
+    
+    
+def SommePai(listeNumber, N):
+    SommePairs = 0
+    i = 0
+    
+    while i< N :
+        S1.acquire()
+        SommePairs = SommePairs + listeNumber[i]
+        i += 2
+        print('Compteur Paire =', i)
+        S2.release()
+    S.acquire()
+    Somme.value += SommePairs
+    print(Somme.value)
+    S.release()
+    
+    
+p1 = mp.Process(target = SommeImp , args = (listeNumber, N, ))
+p2 = mp.Process(target = SommePai , args = (listeNumber, N, ))
+
+p1.start()
+p2.start()
+p1.join()
+p2.join()
+print(Somme.value)
+sys.exit(0)
---
+++ TP5/exo3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 29 15:55:08 2022
+
+@author: terramotu
+Ceci est l'exercice 3 du TP sur les sémaphores
+"""
+
